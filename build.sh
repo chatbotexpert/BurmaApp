a1e59802eb3d5cb417333d7ea11eb3ba0507bcac
Zeeshan Bilal <148781477+chatbotexpert@users.noreply.github.com>
2026-02-18 15:22:56 +0500

    Fix Vercel build script

--- build.sh
+++ build.sh
@@ -3,11 +3,15 @@
 # Exit on error
 set -o errexit
 
-echo "Building the project..."
-python3.12 -m pip install -r requirements.txt
+echo "Creating virtual environment..."
+python3.12 -m venv .venv
+source .venv/bin/activate
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
 
 echo "Collecting static files..."
-python3.12 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 echo "Running migrations..."
-python3.12 manage.py migrate
+python manage.py migrate
